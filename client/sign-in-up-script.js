be7de1089db0cd8bfc09df1d862e2065b220596e
Daniel Nahshonov <danielnahshonov@MacBook-Pro-Daniel.local>
2024-02-26 21:44:30 +0200

    this is ign up page

---
+++ client/sign-in-up-script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container')
+const registerBtn = document.getElementById('register')
+const loginBth = document.getElementById('login')
+
+registerBtn.addEventListener('click',()=>{
+    container.classList.add("active")
+})
+
+loginBth.addEventListener('click',()=>{
+    container.classList.remove("active")
+})
